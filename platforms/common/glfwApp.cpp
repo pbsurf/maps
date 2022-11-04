273a377f97619c978ca7f8784dfae47b9edd21e5
pbsurf <>
2022-11-04 10:27:46 -0700

    cleanup multi-layer maps

--- platforms/common/glfwApp.cpp
+++ platforms/common/glfwApp.cpp
@@ -1519,6 +1519,15 @@ void addGPXPolyline(const char* gpxfile)
   }
 }
 
+std::string yamlToStr(const YAML::Node& node)
+{
+  YAML::Emitter emitter;
+  emitter.SetSeqFormat(YAML::Flow);
+  emitter.SetMapFormat(YAML::Flow);
+  emitter << node;
+  return std::string(emitter.c_str());
+}
+
 class SourceBuilder
 {
 public:
@@ -1527,43 +1536,49 @@ public:
   YAML::Node vectorSrc;
   int order = 0;
 
+  std::vector<std::string> layerkeys;
+
   SourceBuilder(const YAML::Node& s) : sources(s) {}
 
-  void addLayer(const std::string& key, const YAML::Node& src);
+  void addLayer(const std::string& key);  //, const YAML::Node& src);
   void apply();
 };
 
-void SourceBuilder::addLayer(const std::string& key, const YAML::Node& src)
+void SourceBuilder::addLayer(const std::string& key)  //, const YAML::Node& src)
 {
+  YAML::Node src = sources[key];
   if(src["type"].Scalar() == "Multi") {
     for (const auto& layer : src["layers"]) {
       std::string layerkey = layer["source"].Scalar();
-      addLayer(layerkey, sources[layerkey]);
+      addLayer(layerkey);  //, sources[layerkey]);
     }
   }
   else if(src["type"].Scalar() == "Raster") {
+    layerkeys.push_back(key);
     for (const auto& attr : src) {
-      if(attr.first.Scalar() != "title") {
-        YAML::Emitter emitter;
-        emitter.SetSeqFormat(YAML::Flow);
-        emitter.SetMapFormat(YAML::Flow);
-        emitter << attr.second;
-        updates.emplace_back("+sources." + key + "." + attr.first.Scalar(), emitter.c_str());
-      }
+      if(attr.first.Scalar() != "title")
+        updates.emplace_back("+sources." + key + "." + attr.first.Scalar(), yamlToStr(attr.second));
     }
-    // separate style is required for each overlay layer
+    // separate style is required for each overlay layer; overlay layers are always drawn over opaque layers
+    //  text and points are drawn as overlays w/ blend_order -1, so use blend_order < -1 to place rasters
+    //  under vector map text
     std::string style = "raster";
     if(order > 0) {
       style = fstring("overlay-raster-%d", order);
-      updates.emplace_back("+styles." + style, fstring("{base: raster, blend: overlay, blend_order: %d}", order));
+      updates.emplace_back("+styles." + style, fstring("{base: raster, blend: overlay, blend_order: %d}", order-10));
     }
     updates.emplace_back("+layers." + key + ".data.source", key);
     // order is ignored (and may not be required) for raster styles
     updates.emplace_back("+layers." + key + ".draw." + style + ".order", std::to_string(order++));
   }
   else {  // vector map
+    layerkeys.push_back(key);
     vectorSrc = src;  //src["url"].Scalar();
-    order = 9001;  // subsequent rasters should be drawn on top of the vector map
+    ++order;  //order = 9001;  // subsequent rasters should be drawn on top of the vector map
+  }
+
+  for(const auto& update : src["updates"]) {
+    updates.emplace_back("+" + update.first.Scalar(), yamlToStr(update.second));
   }
 }
 
@@ -1587,40 +1602,72 @@ void SourceBuilder::apply()
   loadSceneFile(false, updates);
 }
 
-static void showSourceGUI() {
+// auto it = mapSources.begin();  std::advance(it, currSrcIdx[ii]-1); builder.addLayer(it->first.Scalar(), it->second);
+
+static void showSourceGUI()
+{
   static YAML::Node mapSources = YAML::LoadFile(sourcesFile);
   static constexpr int MAX_SOURCES = 8;
+  static int currIdx = 0;
   static std::vector<int> currSrcIdx(MAX_SOURCES, 0);
   static int nSources = 1;
   static std::string newSrcTitle;
 
-  if (!ImGui::CollapsingHeader("Sources"))
+  if (!ImGui::CollapsingHeader("Sources", ImGuiTreeNodeFlags_DefaultOpen))
     return;
 
   std::vector<std::string> titles = {"None"};
-  for (const auto& src : mapSources)
+  std::vector<std::string> keys = {""};
+  for (const auto& src : mapSources) {
     titles.push_back(src.second["title"].Scalar());
+    keys.push_back(src.first.Scalar());
+  }
 
   std::vector<const char*> ctitles;
   for(const auto& s : titles)
     ctitles.push_back(s.c_str());
 
-  bool reload = false;
+  int reload = 0;
+  if(ImGui::Combo(fstring("Source").c_str(), &currIdx, ctitles.data(), ctitles.size()))
+    reload = 1;  // selected source changed - reload scene
+
+  if(currIdx > 0 && mapSources[keys[currIdx]]["type"].Scalar() == "Multi") {
+    ImGui::SameLine();
+    if (ImGui::Button("Remove"))
+      mapSources.remove(keys[currIdx]);
+  }
+
   for(int ii = 0; ii < nSources; ++ii) {
-    if(ImGui::Combo(fstring("Source %d", ii+1).c_str(), &currSrcIdx[ii], ctitles.data(), ctitles.size()))
-      reload = true;  // selected source changed - reload scene
+    if(ImGui::Combo(fstring("Layer %d", ii+1).c_str(), &currSrcIdx[ii], ctitles.data(), ctitles.size()))
+      reload = 2;  // layer changed - reload scene
   }
 
   if(reload) {
     SourceBuilder builder(mapSources);
-    for(int ii = 0; ii < nSources; ++ii) {
-      if(currSrcIdx[ii] > 0) {
-        auto it = mapSources.begin();
-        std::advance(it, currSrcIdx[ii]-1);
-        builder.addLayer(it->first.Scalar(), it->second);
+    if(reload == 1)
+      builder.addLayer(keys[currIdx]);
+    else {
+      for(int ii = 0; ii < nSources; ++ii) {
+        if(currSrcIdx[ii] > 0)
+          builder.addLayer(keys[currSrcIdx[ii]]);
       }
     }
     builder.apply();
+
+    if(reload == 1 && builder.layerkeys.size() > 1)
+      newSrcTitle = titles[currIdx];
+
+    nSources = std::max(int(builder.layerkeys.size()), nSources);
+    for(int ii = 0; ii < builder.layerkeys.size(); ++ii) {
+      for(int jj = 0; jj < keys.size(); ++jj) {
+        if(builder.layerkeys[ii] == keys[jj]) {
+          currSrcIdx[ii] = jj;
+          break;  // next layer
+        }
+      }
+    }
+    for(int ii = builder.layerkeys.size(); ii < nSources; ++ii)
+      currSrcIdx[ii] = 0;
   }
 
   if (nSources > 1) {
@@ -1632,22 +1679,32 @@ static void showSourceGUI() {
     ++nSources;
 
   if(nSources > 1) {
-    bool ent = ImGui::InputText("Name", &newSrcTitle, ImGuiInputTextFlags_EnterReturnsTrue);
-    ent = ImGui::Button("Save") || ent;
-    if(ent && !newSrcTitle.empty()) {
-      std::fstream fs(sourcesFile, std::fstream::app | std::fstream::binary);
-      std::vector<std::string> newsrc;
-      fs << "\n";
-      fs << "custom-" << mapSources.size() << ":\n";
-      fs << "  type: Multi\n";
-      fs << "  title: " << newSrcTitle << "\n";
-      fs << "  layers:\n";
+    ImGui::InputText("Name", &newSrcTitle, ImGuiInputTextFlags_EnterReturnsTrue);
+    //ent = ImGui::Button("Save") || ent;
+    if(ImGui::Button("Save") && !newSrcTitle.empty()) {
+      std::stringstream fs;  //fs(sourcesFile, std::fstream::app | std::fstream::binary);
+
+      // find available name
+      std::string savekey;
+      if(currIdx > 0 && newSrcTitle == titles[currIdx] && mapSources[keys[currIdx]]["type"].Scalar() == "Multi")
+        savekey = keys[currIdx];
+      else {
+        int ii = mapSources.size();
+        while(ii < INT_MAX && mapSources[fstring("custom-%d", ii)]) ++ii;
+        savekey = fstring("custom-%d", ii);
+        currIdx = keys.size();  // new source will be added at end of list
+      }
+      //YAML::Node node = mapSources[savekey] = YAML::Node(YAML::NodeType::Map);  node["type"] = "Multi";
+      fs << "type: Multi\n";
+      fs << "title: " << newSrcTitle << "\n";
+      fs << "layers:\n";
       for(int ii = 0; ii < nSources; ++ii) {
         if(currSrcIdx[ii] > 0)
-          fs << "    - source: " << mapSources[currSrcIdx[ii]-1].Tag() << "\n";
+          fs << "  - source: " << keys[currSrcIdx[ii]] << "\n";
       }
       newSrcTitle.clear();
-      mapSources = YAML::LoadFile(sourcesFile);  // reload
+      mapSources[savekey] = YAML::Load(fs.str());
+      // we'd set a flag here to save mapsources.yaml on exit
     }
   }
 }
