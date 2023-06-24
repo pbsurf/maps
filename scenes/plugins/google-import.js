28162bdc96c278d4d70efd50c92143d9c22f41be
pbsurf <>
2023-06-24 04:37:15 -0700

    good progress

--- scenes/plugins/google-import.js
+++ scenes/plugins/google-import.js
@@ -3,11 +3,11 @@
 //addUIElement("file_picker", "Input file:", "file");  addUIElement("text_input", "List:", "list");  ...
 //createPluginUI('<g class="horz_layout"><g id="file" class="file_picker"/><g id="list" class="text_input"/></g>');
 
-function importGooglePlaces()
+function importGooglePlaces(url, list)
 {
-  const url = "file:///home/mwhite/maps/Reviews.json";
+  //const url = "file:///home/mwhite/maps/Reviews.json";
   jsonHttpRequest(url, "", function(places) {
-    const list = "Reviews";
+    //const list = "Reviews";
     for(var ii = 0; ii < places.features.length; ii++) {
       const r = places.features[ii];
       const loc = r.properties["Location"];
@@ -22,4 +22,4 @@ function importGooglePlaces()
   });
 }
 
-registerFunction("importGooglePlaces", "command", "Import Google Places");
+registerFunction("importGooglePlaces", "command", "importGooglePlaces(<path to json file>, <name of bookmark list>");
--- scenes/plugins/nominatim-search.js
+++ scenes/plugins/nominatim-search.js
@@ -2,6 +2,7 @@ function nominatimSearch(query, bounds, flags)
 {
   const url = "https://nominatim.openstreetmap.org/search?format=jsonv2&bounded=1&viewbox=" + bounds.join() + "&limit=50&q=" + query;
   jsonHttpRequest(url, "", function(content) {
+    if(content.length >= 50) { flags = flags | 0x8000; }  // MapSearch::MORE_RESULTS flag
     for(var ii = 0; ii < content.length; ii++) {
       const r = content[ii];
       const tags = {"name": r.display_name, [r.category]: r.type};
