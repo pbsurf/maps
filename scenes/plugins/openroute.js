e2fcf6a946e865b6d1ee79c3906c3d516c96d5ac
pbsurf <>
2023-09-18 10:58:10 -0700

    more work on routes

--- scenes/plugins/openroute.js
+++ scenes/plugins/openroute.js
@@ -6,7 +6,7 @@ function openRouteService(mode, waypoints)
   const profile = mode0 == "walk" ? "foot-hiking" : mode0 == "bike" ? "cycling-regular" : "driving-car";
   const url = "https://api.openrouteservice.org/v2/directions/" + profile + "/gpx";
   const hdrs = "Content-Type: application/json\r\nAuthorization: " + secrets.openroute_auth;
-  const body = {"coordinates": waypoints};
+  const body = {"coordinates": waypoints, "elevation": "true"};
 
   httpRequest(url, hdrs, JSON.stringify(body), function(content) {
     addRouteGPX(content);
